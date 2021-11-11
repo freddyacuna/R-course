e14d95274f2e1d46e88ae80b30f9c146a200cf65
Freddy Acuña <42895976+freddyacuna@users.noreply.github.com>
2021-11-11 00:40:54 -0300

    Update Guía 09.R

--- undergraduate/2021 Spring/Guía 09.R
+++ undergraduate/2021 Spring/Guía 09.R
@@ -35,3 +35,23 @@ geom_text_wordcloud_area() +
 scale_size_area(max_size = 20) +
 scale_color_gradient(low = "#fee0d2", high = "#67000d") +
 theme_minimal()
+
+# Pregunta 2
+# Usando los datos de oscar_movies1927_2020.csv que contiene información sobre los Premios Oscar para el
+# período 1927-2020, reponda las siguientes preguntas:
+movies <- read_csv("datos/oscar_movies1927_2020.csv")
+
+# (a) ¿Cuántas observaciones y qué variables contiene la base de datos?
+glimpse(movies)
+
+# (b) Liste los nombres nominados en distintas categorías que comiencen con Richard o James.
+movies %> % pull(name) %> % str_subset(pattern = "ˆ(Richard|James)")
+
+# (c) Presente un listado de películas cuyos títulos comienzan o terminan en cuatro dígitos.
+movies %> % filter(film %in % str_subset(film, pattern = "ˆ\\d{4}|\\d{4}$")) %> %
+distinct(film)
+
+# (d) ¿Cuáles son los diez nombres de pila más frecuentes entre los nominados en la categoría “Mejor actor”?
+movies %> % filter(category == "ACTOR") %> %
+mutate(nombre_pila = str_extract(name, pattern = "ˆ\\w+")) %> %
+count(nombre_pila, sort = TRUE) %> % slice_head(n = 10)
