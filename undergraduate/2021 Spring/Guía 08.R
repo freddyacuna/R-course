1a1fb88aaa9daf84012cbb106cec1f0efda46fc7
Freddy Acuña <42895976+freddyacuna@users.noreply.github.com>
2021-11-11 00:45:08 -0300

    Update Guía 08.R

--- undergraduate/2021 Spring/Guía 08.R
+++ undergraduate/2021 Spring/Guía 08.R
@@ -7,7 +7,7 @@ x <- c("argentina(1)", "Chile", "el salvador", "guatemala", "Honduras", "Uganda"
 # (a) Elimine cualquier caracter que no sea una letra
 country <- str_replace_all(x, pattern = "[\\d\\(\\)]", "")
 country
-str_replace_all(x, pattern = "[ˆa-zA-Z\\s]", "")
+str_replace_all(x, pattern = "[^a-zA-Z\\s]", "")
 
 # (b) Cambia a mayúscula la primera letra de cada uno de los países cuando sea necesario
 country <- str_to_title(country)
@@ -17,20 +17,20 @@ country
 str_to_upper(country)
 
 # (d) ¿Cuántos nombres de países comienzan con una vocal? ¿Qué países comienzan con una vocal?
-sum(str_detect(country,"ˆ[AEIOU]"))
-str_subset(country,"ˆ[AEIOU]")
+sum(str_detect(country,"^[AEIOU]"))
+str_subset(country,"^[AEIOU]")
 
 # (e) ¿Cuántos nombres de países comienzan con una consonante? ¿Qué países comienzan con una consonante?
-sum(str_detect(country,"ˆ[ˆAEIOU]"))
-str_subset(country,"ˆ[ˆAEIOU]")
+sum(str_detect(country,"^[ˆAEIOU]"))
+str_subset(country,"^[^AEIOU]")
 
 # (f) Liste los países cuyos nombres comienzan y terminan con una consonante
-str_subset(country, "ˆ[ˆAEIOU].*[ˆaeiou]$")
-country[str_detect(country, "ˆ[ˆAEIOU].*[ˆaeiou]$")]
+str_subset(country, "^[^AEIOU].*[^aeiou]$")
+country[str_detect(country, "^[^AEIOU].*[^aeiou]$")]
 
 # (g) Liste los países cuyos nombres comienzan con A o U
 
-str_subset(country, "ˆ[AU]")
+str_subset(country, "^[AU]")
 
 # (h) ¿Cuántas letras tiene cada país?
 country
